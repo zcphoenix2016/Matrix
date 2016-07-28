7ce7013114950aca04be4eabb402a1144e58fe5c
zcphoenix2016 <13564240769@139.com>
2016-07-28 09:48:43 +0800

    source file of class Cell

---
+++ Cell.cpp
@@ -0,0 +1,28 @@
+#include "Cell.hpp"
+
+CellState Cell::nextState(unsigned int numOfLiveNeighbours)
+{
+    if(CELLSTATE_DEAD == m_state)
+    {
+        if(3 == numOfLiveNeighbours)
+        {
+            m_state = CELLSTATE_LIVE;
+        }
+    }
+    else
+    {
+        if(2 > numOfLiveNeighbours
+          || 3 < numOfLiveNeighbours)
+        {
+            m_state = CELLSTATE_DEAD;
+        }
+    }
+
+    return m_state;
+}
+
+void Cell::setState(CellState state)
+{
+    m_state = state;
+}
+
